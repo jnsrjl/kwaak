3912b68cf28d41472a6e54128c57558fccad990e
Joonas Rajala <jnsrjl@gmail.com>
2018-02-18 23:41:57 +0200

    Add option to sort by dateTime; Show full information of each sighting

--- src/components/SightingsList.js
+++ src/components/SightingsList.js
@@ -6,29 +6,63 @@ class SightingsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sightings: []
+      sightings: [],
+      descending: true,
     };
   }
 
-  renderSightings() {
+  updateSightings() {
+    // Fetch
     get("/sightings").then(sightings => {
+      // Sort by dateTime
+      sightings.sort((a, b) => {
+        let aMilliseconds = new Date(a.dateTime).getTime();
+        let bMilliseconds = new Date(b.dateTime).getTime();
+        if (this.state.descending) {
+          return aMilliseconds < bMilliseconds;
+        }
+        return aMilliseconds > bMilliseconds;
+      });
+
+      // Set
       this.setState({ sightings: sightings });
+
     }).catch((error) => {
       console.log(error);
     });
+  }
 
+  handleSort = () => {
+    this.setState({
+      sightings: this.state.sightings.reverse(),
+      descending: !this.state.descending
+    });
+  }
+
+  componentWillMount() {
+    this.updateSightings();
+  }
+
+  renderSightings() {
     return this.state.sightings.map((sighting) => (
       <li key={ sighting.id }>
-        { sighting.id } : { sighting.description }
+        <h2>{ new Date(sighting.dateTime).toLocaleString() }</h2>
+        <p>{ sighting.count }x { sighting.species }</p>
+        <p>{ sighting.description }</p>
       </li>
     ));
   }
 
   render() {
     return (
-      <ul>
-        { this.renderSightings() }
-      </ul>
+      <div>
+        <button onClick={ this.handleSort }>
+          { this.state.descending ? "Sort Ascending" : "Sort Descending" }
+        </button>
+        <ul>
+          { this.renderSightings() }
+        </ul>
+      </div>
     )
   }
 }
